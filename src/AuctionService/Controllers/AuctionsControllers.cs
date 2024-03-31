75c28c86c4794d4b80c4921a2c3f52c5344f5e99
neuromagus <neuromagus@mail.ru>
2024-03-31 16:52:03 +0300

    add controller and editor config (this to _this)

---
+++ src/AuctionService/Controllers/AuctionsControllers.cs
@@ -0,0 +1,39 @@
+﻿using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsControllers(AuctionDbContext context, IMapper mapper) : ControllerBase
+{
+    private readonly AuctionDbContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{auctionId}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid auctionId)
+    {
+        var auction = await _context.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == auctionId);
+        
+        if (auction == null) return NotFound();
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+}
