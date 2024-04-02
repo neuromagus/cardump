71d21f274181d93125caddd3f99aba435f0df01e
neuromagus <neuromagus@mail.ru>
2024-04-02 18:19:25 +0300

    added update controller

--- src/AuctionService/Controllers/AuctionsControllers.cs
+++ src/AuctionService/Controllers/AuctionsControllers.cs
@@ -1,4 +1,5 @@
-﻿using AuctionService.Data;
+﻿using AuctionServic.DTOs;
+using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
@@ -25,15 +26,50 @@ public class AuctionsControllers(AuctionDbContext context, IMapper mapper) : Con
         return _mapper.Map<List<AuctionDto>>(auctions);
     }
 
-    [HttpGet("{auctionId}")]
-    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid auctionId)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
     {
         var auction = await _context.Auctions
             .Include(x => x.Item)
-            .FirstOrDefaultAsync(x => x.Id == auctionId);
+            .FirstOrDefaultAsync(x => x.Id == id);
         
         if (auction == null) return NotFound();
 
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        return !result ? BadRequest("Could not save changes to database") 
+            : CreatedAtAction(nameof(GetAuctionById), new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item)
+            .FirstOrDefaultAsync( x => x.Id == id);
+
+        if (auction == null) return NotFound();
+
+        // TODO: check seller name == user name
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        return result ? Ok() : BadRequest("Problem saving changes");
     }
 }
