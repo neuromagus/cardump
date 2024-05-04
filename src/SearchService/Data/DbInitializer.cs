f90b47668200374a5650318efddc5927e615e421
neuromagus <neuromagus@mail.ru>
2024-05-04 17:16:42 +0300

    sync connect to Auction

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,15 +21,12 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("No Data - will attempt to seed");
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDbAsync();
 
-            var itemData = await File.ReadAllTextAsync("Data/Auctions.json");
-            var options = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        Console.WriteLine(items.Count + " returned from the Auction Service");
 
-            await DB.SaveAsync(items);
-        }
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
