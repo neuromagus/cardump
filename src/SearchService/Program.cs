a3e8ae06304edc2b149e638677717a2da41ee0c8
neuromagus <neuromagus@mail.ru>
2024-05-04 19:37:57 +0300

    add hack for sync request from Search to Auction services

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,15 +1,14 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
-using SearchService.Models;
 using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -17,13 +16,25 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-    await DbInitializer.InitDbAsync(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.ToString());
-}
+// add hack for the resend sync (async in here) request again (3 seconds) if data is not accepted 
+// (Auction Service is down or not available)
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try
+    {
+        await DbInitializer.InitDbAsync(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+});
 
 app.Run();
+
+// add hack for the resend sync (async in here) request again (3 seconds) if data is not accepted 
+// (Auction Service is down or not available)
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() 
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
