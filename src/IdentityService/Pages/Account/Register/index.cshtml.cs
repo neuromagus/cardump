75362c80647521ca485481409e0ac57ba3fa6ef4
neuromagus <neuromagus@mail.ru>
2024-05-24 12:24:55 +0300

    create register form

--- src/IdentityService/Pages/Account/Register/index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/index.cshtml.cs
@@ -1,4 +1,8 @@
+using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,10 +10,52 @@ namespace IdentityService.Pages.Register
 {
     [SecurityHeaders]
     [AllowAnonymous]
-    public class Index : PageModel
+    public class Index(UserManager<ApplicationUser> userManager) : PageModel
     {
-        public void OnGet()
+
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+        
+        [BindProperty]
+        public bool RegisterSuccess  { get; set; }
+        
+
+        public IActionResult OnGet(string returnUrl)
         {
+            Input = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl,
+            };
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Input.Button != "register") return Redirect("~/");
+            
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.UserName,
+                    Email = Input.Email,
+                    EmailConfirmed = true,
+                };
+
+                var result = await userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddClaimsAsync(user, [
+                        new Claim(JwtClaimTypes.Name, Input.FullName)
+                    ]);
+
+                    RegisterSuccess = true;
+                }
+            }
+            
+            return Page();
         }
     }
 }
