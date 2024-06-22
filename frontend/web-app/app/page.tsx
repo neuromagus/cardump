34d347c7d1cf4a8189fa2c1265bdb91e2f0cf7a2
neuromagus <neuromagus@mail.ru>
2024-06-22 10:57:12 +0300

    add navbar and icons

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h3 className="text-3xl font-semibold">
-        CarDump app!
-      </h3>
+      content goes here
     </div>
   );
 }
