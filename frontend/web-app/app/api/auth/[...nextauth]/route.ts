c66330b97162b25ef3747beeb95686c4aa4467d0
neuromagus <neuromagus@mail.ru>
2024-07-06 15:56:18 +0300

    last

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,13 @@ export const authOptions: NextAuthOptions = {
             authorization: { params: { scope: "openid profile auctionApp" } },
             idToken: true
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({token, profile, account, user}) {
+            console.log({token, profile, account, user})
+            return token
+        }
+    }
 }
 
 const handler = NextAuth(authOptions)
