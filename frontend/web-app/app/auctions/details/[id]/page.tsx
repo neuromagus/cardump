119dee9fc1b2ffc16107ceec7fcedbee2b7e9260
neuromagus <neuromagus@mail.ru>
2024-07-22 20:15:57 +0300

    add route

---
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Details({params}: {params: {id:string}}) {
+    return (
+        <div>details for {params.id}</div>
+    )
+}
---
+++ frontend/web-app/app/auctions/update/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function Update({params}: {params: {id:string}}) {
+    return (
+        <div>update for {params.id}</div>
+    )
+}
