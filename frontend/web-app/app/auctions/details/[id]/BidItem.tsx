6d376263b60ca65dfa89b5152b327222facc8186
neuromagus <neuromagus@mail.ru>
2024-07-30 02:46:01 +0300

    create bidItem (show in Details page)

---
+++ frontend/web-app/app/auctions/details/[id]/BidItem.tsx
@@ -0,0 +1,56 @@
+import { numberWithComma } from "@/app/lib/numberWithComma"
+import { Bid } from "@/types"
+import { format } from "date-fns/format"
+
+type Props = {
+    bid: Bid
+}
+
+export default function BidItem({bid}: Props) {
+    const getBidInfo = () => {
+        let bgColor = ""
+        let text = ""
+
+        switch (bid.bidStatus) {
+            case "Accepted":
+                bgColor = "bg-green-200"
+                text = "Bid accepted"
+                break;
+            case "AcceptedBelowReserve":
+                bgColor = "bg-amber-500"
+                text = "Reserve not met"
+                break;
+            case "TooLow":
+                bgColor = "bg-red-200"
+                text = "Bid was to low"
+                break;
+            default:
+                bgColor = "bg-red-200"
+                text = "Bid placed after auction finished"
+                break;
+        }
+
+        return {bgColor, text}
+    }
+
+    return (
+        <div className={`
+            border-gray-300 border-2 px-3 py-2 rounded-lg
+            flex justify-between items-center mb-2
+            ${getBidInfo().bgColor}
+        `}>
+            <div className="flex flex-col">
+                <span>Bidder: {bid.bidder}</span>
+                <span className="text-gray-700 text-sm">
+                    Time: {format(new Date(bid.bidTime), "dd mm yyyy h:mm a")}
+                </span>
+            </div>
+            <div className="flex flex-col text-right">
+                <div className="text-xl font-semibold">${numberWithComma(bid.amount)}</div>
+                <div className="flex flex-row items-center">
+                    <span>{getBidInfo().text}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getDetailedViewData } from "@/app/actions/auctionActions"
+import { getBidsForAuction, getDetailedViewData } from "@/app/actions/auctionActions"
 import Heading from "@/app/components/Heading"
 import CountdownTimer from "../../CountdownTimer"
 import CarImage from "../../CarImage"
@@ -6,10 +6,12 @@ import DetailedSpecs from "./DetailedSpecs"
 import { getCurrentUser } from "@/app/actions/authAction"
 import EditButton from "./EditButton"
 import DeleteButton from "./DeleteButton"
+import BidItem from "./BidItem"
 
 export default async function Details({params}: {params: {id:string}}) {
     const data = await getDetailedViewData(params.id)
     const user = await getCurrentUser()
+    const bids = await getBidsForAuction(params.id)
     
     return (
         <div>
@@ -38,6 +40,9 @@ export default async function Details({params}: {params: {id:string}}) {
 
                 <div className="border-2 rounded-lg p-2 bg-gray-100">
                     <Heading title="Bids" />
+                    {bids.map(bid => (
+                        <BidItem key={bid.id} bid={bid} />
+                    ))}
                 </div>
             </div>
 
