e777c85a4a7ce83e7f3f3d2de4180595f40eb02a
neuromagus <neuromagus@mail.ru>
2024-07-29 18:26:26 +0300

    clearing NotificationSrv and add masstransit lib and config appsettings

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,6 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using MassTransit;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x => 
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) => 
+    {
+        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest"));
+        });
+        
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
 
 app.Run();
